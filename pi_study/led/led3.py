c512129a1141a6a242acb057d557fe53a5ad11f9
code1218 <skjil1218@gmail.com>
2025-05-07 17:01:48 +0900

    led실습

---
+++ pi_study/led/led3.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as gpio
+from time import sleep
+
+ledPin = (16, 20, 21)
+
+gpio.setmode(gpio.BCM)
+for pin in ledPin:
+    gpio.setup(pin, gpio.OUT)
+
+try:
+    while True:
+        for pin in ledPin:
+            for i in range(2):
+                gpio.output(pin, gpio.HIGH)
+                sleep(0.1)
+                gpio.output(pin, gpio.LOW)
+                sleep(0.1)
+    
+        
+except KeyboardInterrupt:
+    pass
+finally:
+    gpio.cleanup()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
